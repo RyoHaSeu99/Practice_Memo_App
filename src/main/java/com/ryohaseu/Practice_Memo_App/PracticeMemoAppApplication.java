fdbbc13b7a1b362009383b8cf4feb22d1782fb66
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-21 14:10:50 +0900

    first commit

---
+++ src/main/java/com/ryohaseu/Practice_Memo_App/PracticeMemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.ryohaseu.Practice_Memo_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeMemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryohaseu/Practice_Memo_App/PracticeMemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryohaseu.Practice_Memo_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
